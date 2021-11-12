66a07fec9aa9415aaf79a629155f814b3154fb33
Noudea <oudea.nicolas@gmail.com>
2021-11-12 21:25:21 +0100

    :book: DOC added swagger documentation

---
+++ server/documentation/swagger.js
@@ -0,0 +1,30 @@
+const SwaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Dreamverse API",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ["./server.js"],
+};
+
+
+export {SwaggerOptions}
--- server/server.js
+++ server/server.js
@@ -1,14 +1,41 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDb } from './services/database/connection'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+import { SwaggerOptions } from './documentation/swagger'
 
 dotenv.config()
 const app = express()
 const port = 3000
-
 connectDb()
 
-app.get('/', (req, res) => {
+
+const specs = swaggerJsDoc(SwaggerOptions);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+
+/**
+ * @swagger
+ * /helloWorld:
+ *  get:
+ *    summary: hello world
+ *    description: exemple route
+ *    parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: integer
+ *    responses:
+ *      200:
+ */
+app.get('/helloWorld', (req, res) => {
   res.send('Hello World')
 })
 
