e43805eaa6365245e6c33aed7f3da9fe15d77b61
Noudea <oudea.nicolas@gmail.com>
2021-11-12 19:46:59 +0100

    :wrench: env file

--- server/server.js
+++ server/server.js
@@ -13,6 +13,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(process.env.DB_HOST)
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Dreamverse server listening at http://localhost:${port}`)
 })
